4e8c12703b95d520a04f56fc546af479c0fe7a10
vishal kumar <bhilai100686@gmail.com>
2025-05-22 21:17:05 +0530

    Create PointList.tsx

---
+++ components/components/PointList.tsx
@@ -0,0 +1,14 @@
+// components/PointList.tsx
+
+import React from 'react';
+
+const PointList = () => {
+  return (
+    <div>
+      <h2>Point List Component</h2>
+      {/* Yahan aap apne points list kar sakte hain */}
+    </div>
+  );
+};
+
+export default PointList;
